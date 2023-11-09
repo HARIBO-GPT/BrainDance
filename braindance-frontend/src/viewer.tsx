caaccaada12406a9631c6432a89f5510ccde8cce
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2023-11-10 06:56:06 +0900

    feat: viewer front prototype

--- braindance-frontend/src/viewer.tsx
+++ braindance-frontend/src/viewer.tsx
@@ -26,7 +26,7 @@ const actions = [
 function viewer(){
     return (
         <div>
-            <Grid container style={{paddingBottom: "20px", width: "45vh", margin: "0 auto"}}>
+            <Grid container style={{paddingBottom: "20px", width: "45vh", margin: "0 auto", marginTop:"20px"}}>
             <Grid item>
                 <b style={{fontSize:"30px"}}>모든 브레인댄스</b>
             </Grid>                          
@@ -40,27 +40,30 @@ function viewer(){
             </Grid>
             </Grid>
 
-            <Card style={{ backgroundColor: "#cedbf9", width: "48vh", margin: "0 auto", paddingBottom: "20px", borderRadius: "25px"}}>
-                <Card.Body>
-                    <Card.Title style={{paddingTop:"15px", paddingLeft:"20px", textAlign:"left", fontWeight:"bold", fontSize:"23px", paddingBottom:"5px"}}>객체지향프로그래밍</Card.Title>
-                    <Card.Subtitle style={{paddingLeft: "20px", textAlign:"left"}}>4일 전 업데이트 · 민서님이 만들었어요!</Card.Subtitle>
-                    <Card.Text style={{paddingLeft: "20px", textAlign:"left"}}>
-                    <b>주요 키워드</b>
-                    <br/>브레인스토밍 · 교수님의 롤 실력 
-                    </Card.Text>
-                    
-                    <table style={{width:"45vh", margin: "0 auto"}}>
-                        <ButtonGroup variant="contained" aria-label="outlined primary button group" fullWidth="true">
-                            <Button>퀴즈 풀기</Button>
-                            <Button>삭제</Button>
-                            <Button>자세히 보기</Button>
-                        </ButtonGroup>
-                    </table>
-                </Card.Body>
-            </Card>
+            {[1,2,3].map(i => (
+                <Card style={{ backgroundColor: "#cedbf9", width: "48vh", margin: "0 auto", paddingBottom: "20px", marginBottom: "15px", borderRadius: "25px"}}>
+                    <Card.Body>
+                        <Card.Title style={{paddingTop:"15px", paddingLeft:"20px", textAlign:"left", fontWeight:"bold", fontSize:"23px", paddingBottom:"5px"}}>객체지향프로그래밍</Card.Title>
+                        <Card.Subtitle style={{paddingLeft: "20px", textAlign:"left"}}>4일 전 업데이트 · 민서님이 만들었어요!</Card.Subtitle>
+                        <Card.Text style={{paddingLeft: "20px", textAlign:"left"}}>
+                        <b>주요 키워드</b>
+                        <br/>브레인스토밍 · 교수님의 롤 실력 
+                        </Card.Text>
+                        
+                        <table style={{width:"45vh", margin: "0 auto"}}>
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group" fullWidth="true">
+                                <Button>퀴즈 풀기</Button>
+                                <Button>삭제</Button>
+                                <Button>자세히 보기</Button>
+                            </ButtonGroup>
+                        </table>
+                    </Card.Body>
+                </Card>
+            ))}
+            
 
 
-            <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+            <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1 }}>
             <SpeedDial
                 ariaLabel="SpeedDial basic example"
                 sx={{ position: 'absolute', bottom: 16, right: 16 }}
