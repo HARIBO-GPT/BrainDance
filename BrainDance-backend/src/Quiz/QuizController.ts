b33899d0632e81ca77c89a6ede81acde4cc99ae9
haramjeong <04harams77@gmail.com>
2023-11-11 00:27:46 +0900

    스크립트 프로세싱 & 퀴즈 생성 api

--- BrainDance-backend/src/Quiz/QuizController.ts
+++ BrainDance-backend/src/Quiz/QuizController.ts
@@ -1,12 +1,16 @@
-import { Response } from "express";
-import { UidRequest } from "../interface/user";
-import { ApiResponse } from "../interface/response";
+import { type Response } from "express";
+import { type UidRequest } from "../interface/user";
+import { type ApiResponse } from "../interface/response";
+import { type InputChatGPT, type GPTObjectType, QuizObjectType, QuizInsertObjectType } from "../interface/quiz";
+import { parsingData } from "../openAi/parsing";
+import { insertQuiz } from "../Quiz/QuizService";
 import { admin } from "../auth/firebase";
+import { chatGPT } from "../openAi/openAi";
 
 export const postQuiz = async (req: UidRequest, res: Response): Promise<void> => {
     try {
-        if (typeof req.body.uid === 'string'){
-            const uid: string = req.body.uid;
+        if (typeof req.uid === 'string'){
+            const uid: string = req.uid;
             const userInfo = await admin.auth().getUser(uid);
             if (userInfo === undefined) {
               const resData: ApiResponse = {
@@ -16,11 +20,40 @@ export const postQuiz = async (req: UidRequest, res: Response): Promise<void> =>
               res.status(410).json(resData);
               return;
             }
-
             
+            const passedData: InputChatGPT = {
+                userInput: req.body.quizRawScript,
+                category: req.body.category
+            }
 
+            const processedData: string = await chatGPT(passedData);
 
+            const object: GPTObjectType = await parsingData(processedData);
 
+            const response: ApiResponse = {
+                ok: true,
+                msg: "Successfully POST Quiz About Project",
+                data: object
+            }
+
+            if (object.quiz !== null){
+
+                const quizArray: QuizInsertObjectType[] = []
+                for (const quiz of object.quiz){
+                    if (typeof quiz.quizQuestion === 'string' && typeof quiz.quizAnswer === 'string' && typeof quiz.quizComment === 'string'){
+                        const item: QuizInsertObjectType = {
+                            projectId: req.body.projectId,
+                            quizQuestion: quiz.quizQuestion,
+                            quizAnswer: quiz.quizAnswer,
+                            quizComment: quiz.quizComment
+                        }
+                        quizArray.push(item);
+                    }
+                }
+                await insertQuiz(quizArray);
+            }
+
+            res.status(200).json(response);
 
         }
     }
---
+++ BrainDance-backend/src/openAi/parsing.ts
@@ -0,0 +1,94 @@
+import { type GPTObjectType, type QuizObjectType } from "../interface/quiz";
+
+export const parsingData = async (processedData: string): Promise<GPTObjectType> => {
+    let summaryReady: boolean = false;
+    let keywordReady: boolean = false;
+    let qnaReady: boolean = false;
+
+    let qnaNumber: number = 0;
+
+    const summaryStr: string = "요약";
+    const keywordStr: string = "키워드";
+    const qnaStr: string = "질문과 답변";
+
+    const lst: string[] = processedData.split('\n');
+    let temp: string = '';
+
+    const gptObject: GPTObjectType = {
+        summaryText: null,
+        keyword:  null,
+        quiz: null
+    }
+
+    const quizArray: QuizObjectType[] = [];
+
+    let quizObject: QuizObjectType = {
+        quizQuestion: null,
+        quizAnswer: null,
+        quizComment: null
+    }
+
+    const answerPattern: RegExp = /- 답변(.*)/;
+    const commentPattern: RegExp = /- 의도(.*)/;
+
+    for (const i of lst){
+        if (i.substring(0, 3) === "###") {
+            temp = i.substring(3);
+            if (temp.includes(summaryStr)) {
+                summaryReady = true;
+            }
+            else if (temp.includes(keywordStr)) {
+                keywordReady = true;
+            }
+            else if (temp.includes(qnaStr)) {
+                qnaReady = true;
+                qnaNumber++;
+            }
+        }
+        else if (summaryReady){
+            gptObject.summaryText = i;
+            summaryReady = false;
+        }
+        else if (keywordReady){
+            const keyword: string[] = i.split(", ");
+            gptObject.keyword = keyword;
+            keywordReady = false;
+        }
+        else if (qnaReady){
+            if (quizObject.quizAnswer !== null && quizObject.quizComment !== null && quizObject.quizQuestion !== null) {
+                quizArray.push(quizObject); // Push the completed quizObject into the quizArray
+                quizObject = {
+                    quizQuestion: null,
+                    quizAnswer: null,
+                    quizComment: null
+                };
+                qnaNumber++;
+            }
+            if (i.substring(0,2).includes(qnaNumber.toString()) && quizObject.quizQuestion === null){
+                quizObject.quizQuestion = i;
+            }
+            else if (i.substring(0,2) === "  " && !(i.includes("- 답변")) && !(i.includes("- 의도")) &&  quizObject.quizQuestion !== null){
+                quizObject.quizQuestion += '\n' + i;
+            }
+            else if (i.substring(0,2) === "  " && (i.includes("- 답변"))){
+                let matchResult = i.match(answerPattern);
+                if (matchResult && matchResult.length > 1) {
+                    let extractedString: string = matchResult[1];
+                    quizObject.quizAnswer = extractedString
+                }
+            }
+            else if (i.substring(0,2) === "  " && (i.includes("- 의도") )){
+                let matchResult = i.match(commentPattern);
+                if (matchResult && matchResult.length > 1) {
+                    let extractedString: string = matchResult[1];
+                    quizObject.quizComment = extractedString;
+                }
+            }
+        }
+    }
+    quizArray.push(quizObject); 
+
+    gptObject.quiz = quizArray;
+
+    return gptObject;
+}
