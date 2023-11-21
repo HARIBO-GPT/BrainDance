83540d1402a2f887d9c7fa7d89e0eb9472688551
haramjeong <04harams77@gmail.com>
2023-11-21 16:26:04 +0900

    feat : youtube 영상 추천 함수 추가

---
+++ BrainDance-backend/src/youtube/crawling.ts
@@ -0,0 +1,31 @@
+import { google } from 'googleapis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface SearchType {
+    q: string;
+    type: string;
+    part: string;
+    maxResults: number;
+}
+
+const apiKey = process.env.YOUTUBE_API_KEY;
+
+export async function getYoutubeVideos(query: string[], maxResults: number = 5): Promise<string[]> {
+  const youtube = google.youtube({ version: 'v3', auth: apiKey });
+  const searchQuery: string = query.join(' ');
+  const response = await youtube.search.list({
+    q: searchQuery,
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: maxResults
+  } as any);
+
+  const videoUrls: string[] = response.data.items?.map((item) => {
+    const videoId = item.id?.videoId;
+    return videoId ? `https://www.youtube.com/watch?v=${videoId}` : '';
+  }) || [];
+
+  return videoUrls;
+}
